38958f74fe0c0cf37501fc879209a9d3df242811
evan <iftekhar.4826@yahoo.com>
2025-01-16 00:43:40 +0600

    Practice

--- start.py
+++ start.py
@@ -47,3 +47,43 @@ i=1
 while i<=5:
     print(i,end=" ")
     i += 1
+
+#store data
+#===================================
+#list
+print()
+food=["Pizza","Burger"]
+print(food)
+food.append("Pasta")
+print(food)
+
+num_list=[1,2,3,4,5]
+sum_num=sum(num_list)
+print("Summation: ",sum_num)
+
+
+#Tuples
+
+point=(3,4)
+x,y=point
+print("(x,y)= ",x,y)
+
+fruits=("apple","orange","banana","mango")
+print("Fruits: ",fruits)
+
+
+#Set(curly_braces)
+
+car={"BMW","Audi","Prado","BMW"}
+print("Car: ",car)
+
+#Dictionary
+
+bike={"Owner":"Evan","Company":"Honda"}
+print("Bike: ",bike)
+
+print(bike["Owner"])#specification
+
+bike["Company"]="Yamaha" #data modified
+print("Bike: ",bike)
+
